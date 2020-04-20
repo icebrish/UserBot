ed7b6705615083fdf7dc1fcce2f65fd7bb18e568
icebrish <63074712+icebrish@users.noreply.github.com>
2020-04-20 14:36:43 -0700

    Update peep.py

--- userbot/modules/peep.py
+++ userbot/modules/peep.py
@@ -30,12 +30,12 @@ async def _(event):
         animation_chars = [
             
             "\n 🖤🖤LILPEEP BEAMER-BOY🖤🖤 ",
-            "\n Man, I don't know what the fuck goin' on lately,\n bro Everybody actin' real different and shit, ",
-            "\n Ain't nobody, nobody was talkin' to me like a few months ago \n And now everybody hittin' my phone up and shit",    
+            "\n Man, I don't know what the fuck goin' on lately,\n bro Everybody actin' real different and shit",
+            "\n Ain't nobody,nobody was talkin'to me like a few months ago \n And now everybody hittin' my phone up and shit",    
             "\n I'm a mothafuckin' schema boy, I'm a dreamer boy \n I love a girl that don't even fuckin' need a boy",
             "\n Baby, I'm a beamer boy, I need a beamer, boy \n I want a Z3, that's a two-seater, boy",
             "\n Okay, I pull my cash out, shawty pass out \n Take her ass out, then I spaz out ",    
-            "\n Okay, yeah, I hit that, shawty, get back \n I got death notes, where my list at?",
+            "\n Okay, yeah, I hit that, shawty,get back \n I got death notes, where my list at?",
             "\n Yeah, I'm in my zone now, I put my phone down \n I'm on my own now, I'm on my own now",
             "\n Ya girl, she wanna go down on a real one \n I hit JGRXXN, like \"What's the deal bruh?\"",    
             "\n You see me doin' shows now, I'm a pro now \n I got hoes now and I got some dough now",
