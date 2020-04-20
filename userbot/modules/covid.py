7b7dec9a8bc7548c0adac351abfd87d03ee34ee9
icebrish <63074712+icebrish@users.noreply.github.com>
2020-04-20 09:08:30 -0700

    Update covid.py

--- userbot/modules/covid.py
+++ userbot/modules/covid.py
@@ -17,12 +17,12 @@ async def corona(event):
     covid = Covid()
     country_data = covid.get_status_by_country_name(country)
     if country_data:
-        output_text =  f"`Confirmed   : {country_data['confirmed']}`\n"
-        output_text += f"`Active      : {country_data['active']}`\n"
-        output_text += f"`Deaths      : {country_data['deaths']}`\n"
-        output_text += f"`Recovered   : {country_data['recovered']}`\n"
+        output_text =  f"`Confirmed🤕  : {country_data['confirmed']}`\n"
+        output_text += f"`Active🤧     : {country_data['active']}`\n"
+        output_text += f"`Deaths💀     : {country_data['deaths']}`\n"
+        output_text += f"`Recovered😇   : {country_data['recovered']}`\n"
         output_text += (
-            "`Last update : "
+            "`Last update🧭 : "
             f"{datetime.utcfromtimestamp(country_data['last_update'] // 1000).strftime('%Y-%m-%d %H:%M:%S')}`\n"
         )
         output_text += f"Data provided by [Johns Hopkins University](https://j.mp/2xf6oxF)"
